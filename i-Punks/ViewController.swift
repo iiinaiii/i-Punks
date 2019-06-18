2793f09492b57dff2a6d5239a016f194c96378f6
naoki-ishii <kyoumi155@gmail.com>
2019-06-18 22:18:56 +0900

    Initial Commit

---
+++ i-Punks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-Punks
+//
+//  Created by 石井直貴 on 2019/06/18.
+//  Copyright © 2019 Naoki Ishii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i-PunksTests/i_PunksTests.swift
@@ -0,0 +1,34 @@
+//
+//  i_PunksTests.swift
+//  i-PunksTests
+//
+//  Created by 石井直貴 on 2019/06/18.
+//  Copyright © 2019 Naoki Ishii. All rights reserved.
+//
+
+import XCTest
+@testable import i_Punks
+
+class i_PunksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
